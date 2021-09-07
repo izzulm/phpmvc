5caf4041f29e7c33df77712d9d5c904ae5fb439f
izzulm <muhizzul16@gmail.com>
2021-09-07 10:58:13 +0700

    pertama sampai routing

---
+++ app/core/App.php
@@ -0,0 +1,25 @@
+<?php 
+
+/**
+ * 
+ Ini buat dokumentasi
+ */
+class App
+{
+    public function __construct(){
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    public function parseURL(){
+        if( isset($_GET['url']) ){
+            $url = rtrim($_GET['url'], '/' );
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
+    }
+
+}
+
+ ?>
