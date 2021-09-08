0faf822eb8b91eee16b2660b89b88c8e58f58a24
izzulm <muhizzul16@gmail.com>
2021-09-08 09:35:44 +0700

    Belajar assets & masukin bootstrap

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,6 +1,10 @@
-
-    <h1>Ini Halaman ABout Gengs!</h1>
+<div class="container">
+    <h1>Ini Halaman About Gengs!</h1>
+    <img src="<?= BASEURL; ?>/img/Ijuj.JPG" alt="Izzul" width="200" height="180" class="rounded-circle shadow">
+    <br><br>
     <ul>
         <li>Halo Nama Saya <?= $data['nama'] ?></li>
         <li>dan pekerjaan saya adalah <?= $data['pekerjaan'] ?>! </li>
+        <li><a href="<?= BASEURL; ?>/about/page" type="button" class="btn btn-outline-success">Pages</a></li>
     </ul>
+</div>
