38457bd0242acb5b1fe001940e1f6afbca991e07
izzulm <muhizzul16@gmail.com>
2021-09-08 17:45:32 +0700

    set detail mahasiswa

--- app/controllers/Mahasiswa.php
+++ app/controllers/Mahasiswa.php
@@ -9,6 +9,14 @@ class Mahasiswa extends Controller {
         $this->view('mahasiswa/index', $data);
         $this->view('templates/footer');
     }
+    public function detail($id) {
+        $data['judul'] = 'Mahasiswa';
+        $data['mhs']= $this->model('Mahasiswa_model')->getMahasiswaById($id);
+
+        $this->view('templates/header', $data);
+        $this->view('mahasiswa/detail', $data);
+        $this->view('templates/footer');
+    }
 }
 
  ?>
--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -13,6 +13,12 @@ class Mahasiswa_model {
         $this->db->query('SELECT * FROM mahasiswa');
         return $this->db->resultSet();
     }
+
+    public function getMahasiswaById($id){
+        $this->db->query('SELECT * FROM '.$this->table. ' WHERE id=:id');
+        $this->db->bind('id', $id);
+        return $this->db->single();
+    }
 }
 
 
